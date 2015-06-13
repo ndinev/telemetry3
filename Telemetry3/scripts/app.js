7e33d65e2566ac1814dc6a4bfbf24aecc6d89dcc
Neven Dinev <ndinev@gmail.com>
2015-06-13 18:44:35 +0000

    Added door view Improve naming

--- Telemetry3/scripts/app.js
+++ Telemetry3/scripts/app.js
@@ -1,4 +1,3 @@
-
 (function () {
 
     // store a reference to the application object that will be created
@@ -7,44 +6,39 @@
 
     // create an object to store the models for each view
     window.APP = {
-      models: {
-        home: {
-          title: 'Home'
-        },
-        settings: {
-          title: 'Settings'
-        },
-        contacts: {
-          title: 'Contacts',
-          ds: new kendo.data.DataSource({
-            data: [{ id: 1, name: 'Bob' }, { id: 2, name: 'Mary' }, { id: 3, name: 'John' }]
-          }),
-          alert: function(e) {
-            alert(e.data.name);
-          }
+        models: {
+            temp: {
+                title: 'Temp'
+            },
+            voltage: {
+                title: 'Voltage'
+            },
+            door: {
+                title: 'Door'
+            }
+
         }
-      }
-    };
+    }
 
     // this function is called by Cordova when the application is loaded by the device
-    document.addEventListener('deviceready', function () {  
-      
-      // hide the splash screen as soon as the app is ready. otherwise
-      // Cordova will wait 5 very long seconds to do it for you.
-      navigator.splashscreen.hide();
+    document.addEventListener('deviceready', function () {
 
-      app = new kendo.mobile.Application(document.body, {
-        
-        // you can change the default transition (slide, zoom or fade)
-        transition: 'slide',
-        
-        // comment out the following line to get a UI which matches the look
-        // and feel of the operating system
-        skin: 'flat',
+        // hide the splash screen as soon as the app is ready. otherwise
+        // Cordova will wait 5 very long seconds to do it for you.
+        navigator.splashscreen.hide();
 
-        // the application needs to know which view to load first
-        initial: 'views/home.html'
-      });
+        app = new kendo.mobile.Application(document.body, {
+
+            // you can change the default transition (slide, zoom or fade)
+            transition: 'slide',
+
+            // comment out the following line to get a UI which matches the look
+            // and feel of the operating system
+            skin: 'flat',
+
+            // the application needs to know which view to load first
+            initial: 'views/temp.html'
+        });
 
     }, false);
 
